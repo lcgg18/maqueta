bef8bbec053e5c31bdd62edf4707add7d15d0773
LuisGuerra <lcgg18@gmail.com>
2021-09-21 21:57:05 -0500

    segundo commit

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+window.onload = iniciar;
+
+
+
+function iniciar() {
+  var btnGuardar = document.getElementById("btnGuardar");
+  btnGuardar.addEventListener("click", clickBtnGuardar);
+}
+
+function clickBtnGuardar() {
+  alert("Se guardaron los correctamente");
+}
