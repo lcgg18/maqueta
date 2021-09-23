e884c224802ae69b9aec46200d307bb259ba1080
LuisGuerra <lcgg18@gmail.com>
2021-09-22 21:52:10 -0500

    quinto commit

--- javascript/index.js
+++ javascript/index.js
@@ -1,27 +1,34 @@
 window.onload = iniciar;
-window.onload = limpiar;
-
-
 
 function iniciar() {
   var btnGuardar = document.getElementById("btnGuardar");
   btnGuardar.addEventListener("click", clickBtnGuardar);
- 
+
+  var btnBorrar = document.getElementById("btnBorrar");
+  btnBorrar.addEventListener("click", clickLimpiarFormulario);
+  
 }
+
+// function btnGuardar(){
+//   var btnGuardar = document.getElementById("btnGuardar");
+//   btnGuardar.addEventListener("click", clickBtnGuardar);
+// }
 
 function clickBtnGuardar() {
   alert("Se guardaron los correctamente");
 }
 
-function limpiar() {
-  alert("si nicial limpiar");
-  var btnBorrar = document.getElementById("btnBorrar");
-  btnBorrar.addEventListener("click", clickLimpiarFormulario);
-}
+// function btnBorrar() {
+//   var btnBorrar = document.getElementById("btnBorrar");
+//   btnBorrar.addEventListener("click", clickLimpiarFormulario);
+// }
 
 function clickLimpiarFormulario() {
   document.getElementById("inputAct").value="";
-  
-  alert("Se borro los correctamente");
-  
+  document.getElementById("inputAct6").value="";
+  document.getElementById("inputAct2").value="";
+  document.getElementById("inputAct3").value="";
+  document.getElementById("inputAct4").value="";
+  document.getElementById("inputAct5").value="";
+      
 }
