9654982be95e2225cab46252d37268e162171568
LuisGuerra <lcgg18@gmail.com>
2021-09-22 20:08:15 -0500

    cuarto commit

--- javascript/index.js
+++ javascript/index.js
@@ -1,12 +1,27 @@
 window.onload = iniciar;
+window.onload = limpiar;
 
 
 
 function iniciar() {
   var btnGuardar = document.getElementById("btnGuardar");
   btnGuardar.addEventListener("click", clickBtnGuardar);
+ 
 }
 
 function clickBtnGuardar() {
   alert("Se guardaron los correctamente");
+}
+
+function limpiar() {
+  alert("si nicial limpiar");
+  var btnBorrar = document.getElementById("btnBorrar");
+  btnBorrar.addEventListener("click", clickLimpiarFormulario);
+}
+
+function clickLimpiarFormulario() {
+  document.getElementById("inputAct").value="";
+  
+  alert("Se borro los correctamente");
+  
 }
